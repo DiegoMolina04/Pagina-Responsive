01995b83abb5002ed06a1594d79e85c6008ce787
Diego Molina <diegomolina_30@hotmail.com>
2021-09-26 18:52:44 -0500

    Desarrollo slides

--- js/main.js
+++ js/main.js
@@ -7,3 +7,34 @@ $(window).scroll(function(){
         $('.header-1').addClass('fixedbar');
     }
 })
+
+//Funcion para delizar hacia los lados cada slide
+var slideIndex=1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n){
+    var i;
+    /*Trae todos los elementos con esa misma clase*/
+    var slides = document.getElementsByClassName("myslides");
+  
+    if(n > slides.length){
+        slideIndex = 1
+    }
+
+    if(n < 1){
+
+        slideIndex = slides.length
+
+    }
+
+    for (i=0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        
+    }
+
+    slides[slideIndex-1].style.display ="block";
+}
